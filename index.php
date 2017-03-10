959dcb78b341224334a214a2f701221c2056ceaf
Bruce <brucev@theedge.ca>
2017-03-10 16:31:05 -0700

    First files for custom WP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+  while (have_posts()) : the_post(); ?>
+
+<article class="post">
+  <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h1></a>
+  <?php the_content(); ?>
+</article>
+
+<?php endwhile;
+
+else :
+  echo '<p>No Content Found</p>';
+
+endif;
+
+get_footer();
+
+?>
